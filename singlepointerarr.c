8c1140e2a58c93d8d65355f458fc48ead225ff1e
^____^ <hys2rang@gmail.com>
2019-06-11 15:34:11 +0900

    Update singlepointerarr.c

--- singlepointerarr.c
+++ singlepointerarr.c
@@ -3,14 +3,16 @@
 #include <time.h>
 #pragma warning(disable:4996)
 
-int main()
-{
-	//int arr[5] = {3,4,1,5,2}; 배열 a를 보는 포인터 변수 p를 선언하고 p를 사용하여 a내ㅜ의 요소들을 출력하시오
-	int arr[5] = { 3,4,1,5,2 };
-	int *p = arr;
+void print_arr(int* p){
 	for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
 		printf("%d ", *(p + i));
     //*(p + i) == p[i] == arr[i] == *(arr+i)
 	}
+}
+int main()
+{
+	//int arr[5] = {3,4,1,5,2}; 배열 a를 보는 포인터 변수 p를 선언하고 p를 사용하여 a내ㅜ의 요소들을 출력하시오
+	int arr[5] = { 3,4,1,5,2 };
+	print_arr(arr);
 	return 0;
 }
