0103ba34319cf8e2d83a825898dc99de907ffaab
^____^ <hys2rang@gmail.com>
2019-06-12 17:42:40 +0900

    Update ptrswap.c

--- ptrswap.c
+++ ptrswap.c
@@ -1,54 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 #pragma warning(disable:4996)
-void swap1(int *a, int *b) { //main에 위치한 a, b 변수의 주소를 전달 받아 a,b 값을 교환하는 함수
+
+//main에 위치한 a,b 변수의 주소를 전달 받아 a,b값을 교환하는 함수
+void swap1(int *a, int *b)
+{
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
-void swap2(int **ap, int **bp) {//ap, bp 변수의 주소를 전달 받아 a, b값을 교환하는 함수
+//ap, bp 변수의 주소를 전달 받아 a, b 값을 교환하는 함수
+void swap2(int **ap, int **bp)
+{
 	int tmp = **ap;
 	**ap = **bp;
 	**bp = tmp;
 }
-void swap3(int ***app, int ***bpp) {//app, bpp 변수의 주소를 받아 a, b의 값을 교환하는 함수
+//app, bpp 변수의 주소를 전달 받아 a, b 값을 교환하는 함수
+void swap3(int ***app, int ***bpp)
+{
 	int tmp = ***app;
 	***app = ***bpp;
 	***bpp = tmp;
 }
-void swap4(int **ap, int **bp) {//ap, bp 변수의 주소를 전달 받아 ap, bp 값을 교환하는 함수
-	int* tmp= *ap;
+//ap, bp 변수의 주소를 전달 받아 ap, bp 값을 교환하는 함수
+void swap4(int **ap, int **bp)
+{
+	int *tmp = *ap;
 	*ap = *bp;
 	*bp = tmp;
 }
-void swap5(int ***app, int ***bpp) {//app, bpp 변수의 주소를 전달 받아, ap, bp 값을 교환하는 함수
+//app, bpp 변수의 주소를 전달 받아 ap, bp 값을 교환하는 함수
+void swap5(int ***app, int ***bpp)
+{
 	int *tmp = **app;
 	**app = **bpp;
 	**bpp = tmp;
 }
-void swap6(int ***app, int ***bpp) {//app, bpp 변수의 주소를 받아, app, bpp 값을 교환하는 함수
-	int tmp = ***app;
-	***app = ***bpp;
-	***bpp = tmp;
+//app, bpp 변수의 주소를 전달 받아 app, bpp 값을 교환하는 함수
+void swap6(int ***app, int ***bpp)
+{
+	int *tmp = *app;
+	*app = *bpp;
+	*bpp = tmp;
 }
-int main()
+int main(int argc, char** argv)
 {
 	int a = 10, b = 20;
 	int *ap = &a, *bp = &b;
 	int **app = &ap;
 	int **bpp = &bp;
-
+	
 	swap1(&a, &b);
-	printf("a : %d a : %d\n",a, b);
+	printf("a : %d b : %d\n", a, b);
 	swap2(&ap, &bp);
-	printf("a : %d a : %d\n",a, b);
+	printf("a : %d b : %d\n", a, b);
 	swap3(&app, &bpp);
-	printf("a : %d a : %d\n",a, b);
+	printf("a : %d b : %d\n", a, b);
 	swap4(&ap, &bp);
-	printf("ap : %d bp : %d\n", *ap, *bp);
+	printf("a : %d b : %d\n", *ap, *bp);
 	swap5(&app, &bpp);
-	printf("ap : %d bp : %d\n", *ap, *bp);
-	swap6(&app, &bpp);
-	printf("app : %d bpp : %d\n", **app, **bpp);
+	printf("a : %d b : %d\n", *ap, *bp);
+	swap6(&a, &b);
+	printf("a : %d b : %d\n", **app, **bpp);
+
 	return 0;
 }
