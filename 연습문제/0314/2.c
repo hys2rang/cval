d88b7b860ac7ae5c8f27fbc52fc0059757da82bd
^____^ <hys2rang@gmail.com>
2019-06-13 16:47:17 +0900

    Create 2.c

---
+++ 연습문제/0314/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main(int argc, char** argv)
+{
+	printf("대학생활을 행복하고\n보람있게 보내길 바래요.");
+	return 0;
+}
