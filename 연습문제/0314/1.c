31ed0c384145d5cc00ab57b1b049f56704224ef5
^____^ <hys2rang@gmail.com>
2019-06-13 16:46:22 +0900

    Create 1.c

---
+++ 연습문제/0314/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable:4996)
+
+int main(int argc, char** argv)
+{
+	printf("전자공학과 학생들이\n");
+	printf("모두 컴퓨터프로그래밍을\n");
+	printf("잘 할 수 있기를 바랍니다.");
+	return 0;
+}
