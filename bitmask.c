f586a19eed95ad5da85454e389294893f4da5904
^____^ <hys2rang@gmail.com>
2019-06-15 16:21:04 +0900

    Create bitmask.c

---
+++ bitmask.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+int main(int argc, char** argv)
+{
+	int a = 0xA5;
+
+	if (a & (1 << 6)) {
+		puts("true");
+	}
+	else {
+		puts("false");
+	}
+	a = a | (5 << 1);
+	printf("%x\n", a);
+
+	a = a & (~6);
+	printf("%x\n", a);
+
+	printf("%x\n", a&(7 << 5));
+
+	return 0;
+}
